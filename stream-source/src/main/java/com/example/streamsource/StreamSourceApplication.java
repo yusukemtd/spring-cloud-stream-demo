cd06a79f24dd6d85eb55206a818a027978276790
Yusuke Matsuda <yusuke.madd@gmail.com>
2018-06-25 23:50:52 +0900

    first commit

---
+++ stream-source/src/main/java/com/example/streamsource/StreamSourceApplication.java
@@ -0,0 +1,12 @@
+package com.example.streamsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamSourceApplication.class, args);
+	}
+}
