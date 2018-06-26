7a47e54ad372012b64fca7133dfc522026630dc2
Yusuke Matsuda <yusuke.madd@gmail.com>
2018-06-27 00:57:12 +0900

    Add sink stream

---
+++ stream-sink/src/main/java/com/example/streamsink/StreamSinkApplication.java
@@ -0,0 +1,12 @@
+package com.example.streamsink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamSinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamSinkApplication.class, args);
+	}
+}
