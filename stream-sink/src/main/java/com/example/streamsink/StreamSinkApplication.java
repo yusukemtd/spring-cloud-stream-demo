b80af58ad2de9fb0b8ea453e7cac1db4f528fedc
Yusuke Matsuda <yusuke.madd@gmail.com>
2018-07-08 22:43:54 +0900

    Implement sink stream

--- stream-sink/src/main/java/com/example/streamsink/StreamSinkApplication.java
+++ stream-sink/src/main/java/com/example/streamsink/StreamSinkApplication.java
@@ -2,11 +2,24 @@ package com.example.streamsink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 
 @SpringBootApplication
+@EnableBinding(Sink.class)
 public class StreamSinkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StreamSinkApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(StreamSinkApplication.class, args);
+  }
+
+  @StreamListener(Sink.INPUT)
+  public void print(Tweet tweet) {
+    System.out.println("Received " + tweet.text);
+  }
+
+  public static class Tweet {
+    public String text;
+  }
 }
