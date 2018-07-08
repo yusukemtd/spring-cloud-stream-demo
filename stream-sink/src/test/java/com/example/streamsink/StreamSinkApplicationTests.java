a8085a44f6ce26c0459cdcb0a6fe9c15b484cf57
Yusuke Matsuda <yusuke.madd@gmail.com>
2018-07-09 00:32:31 +0900

    Add stream sink test

--- stream-sink/src/test/java/com/example/streamsink/StreamSinkApplicationTests.java
+++ stream-sink/src/test/java/com/example/streamsink/StreamSinkApplicationTests.java
@@ -1,16 +1,33 @@
 package com.example.streamsink;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.rule.OutputCapture;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.integration.support.MessageBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.example.streamsink.StreamSinkApplication.Tweet;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class StreamSinkApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Autowired
+  Sink sink;
+  @Rule
+  public OutputCapture capture = new OutputCapture();
+
+  @Test
+  public void tweetTest() {
+    Tweet tweet = new Tweet();
+    tweet.text = "test";
+    sink.input().send(MessageBuilder.withPayload(tweet).build());
+    
+    assertThat(capture.toString()).isEqualTo("Received test" + System.lineSeparator());
+  }
 
 }
